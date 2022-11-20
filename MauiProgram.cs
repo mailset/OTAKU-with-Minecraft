a70850086614928d63375c90a6b250be11ed8c8b
mail_set <m_s114514@outlook.com>
2022-11-20 21:51:43 +0800

    添加了一些文字还改了配色www

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,10 @@
-﻿namespace OTAKU_with_Minecraft;
+﻿/**
+  * @Author :tmdakm mail_set IloveEi 
+  * @Date :2022/11/20 21:00
+  * @Git :https://github.com/mailset/OTAKU-with-Minecraft
+  */
+
+namespace OTAKU_with_Minecraft;
 
 public static class MauiProgram
 {
